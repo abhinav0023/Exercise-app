3d4fd522e2a7a93564a5306faee477d5f834a7a0
Abhinav <abhinavhooda223@gmail.com>
2024-01-01 13:36:31 +0530

    done till fetching api but causing error

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,16 @@
-import React from 'react'
+// HeroBanner.js
 
-function Hero() {
+import React from 'react';
+import './styling/Hero.css';
+
+function HeroBanner() {
   return (
-    <div>Hero</div>
-  )
+    <div className="hero-banner">
+      <h1>Your Exercise App</h1>
+      <p>Transform your body, one workout at a time.</p>
+      <button>Get Started</button>
+    </div>
+  );
 }
 
-export default Hero
+export default HeroBanner;
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import "./styling/SearchExercises.css";
+import React, { useEffect, useState } from "react";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+
+
 
 function SearchExercises() {
+  const [search, setSearch] = useState("");
+  const[exercises, setExercises] = useState([])
+  const[bodyParts,setBodyParts] =useState([])
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+
+      setBodyParts(['all', ...bodyPartsData]);
+    };
+    
+    fetchExercisesData();
+  }, []);
+  const handleSearch = async () => {
+    if (search) {
+      const exerciseData = await fetchData(
+        'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+        exerciseOptions
+      );
+  
+      // Filter exercises based on search criteria
+      const searchedExercises = exerciseData.filter(
+        (exercise) =>
+          exercise.name.toLowerCase().includes(search) ||
+          exercise.equipment.toLowerCase().includes(search) ||
+          exercise.bodyPart.toLowerCase().includes(search) ||
+          exercise.target.toLowerCase().includes(search)
+      );
+  
+      // Update state with the searched exercises
+      setExercises(searchedExercises);
+      console.log(searchedExercises);
+  
+      // Clear the search input after performing the search
+      setSearch('');
+    }
+  };
+  
+  
   return (
-    <div>SearchExercises</div>
-  )
+    <div className="search-container">
+      <input
+        value={search}
+        onChange={(e) => setSearch(e.target.value.toLowerCase())}
+        type="text"
+        placeholder="Search exercises..."
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
 }
 
-export default SearchExercises
+export default SearchExercises;
